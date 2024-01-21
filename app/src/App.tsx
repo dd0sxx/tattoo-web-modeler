56846396d7859b034706ecd040654135a46a438d
dd0sxx <contacttheotelonis@gmail.com>
2024-01-21 15:53:02 +0100

    prettier

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Canvas } from '@react-three/fiber'
 
 function App() {
   return (
+    <>
     <h1 className="text-3xl font-bold underline text-red-600">
       Simple React Typescript Tailwind Sample
     </h1>
+    <div id="canvas-container">
+     <Canvas />
+    </div>
+    </>
   );  
 }
 
